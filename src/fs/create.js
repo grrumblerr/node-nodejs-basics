9c9d540804de9e77e91152f50dae256c929edb51
grrumblerr <grrumblerr@yandex.by>
2022-12-01 22:10:38 +0300

    feat: add fs/create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,10 @@
+import {writeFile} from 'fs';
 const create = async () => {
     // Write your code here 
+    
+    writeFile('./src/fs/files/fresh.txt', 'I am fresh and young', {flag: 'wx'}, function (err) {
+        if (err) throw new Error ('FS operation failed')
+    })
 };
 
 await create();
